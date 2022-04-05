7532e504276c321b8bca393c876ae9a4ccaa594c
juananre <78058130+juananre@users.noreply.github.com>
2022-04-05 15:23:34 -0500

    me muero ayuda

--- Taller 2/Taller_2/UnitTest1.cs
+++ Taller 2/Taller_2/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System;
 
 namespace Taller_2
@@ -6,21 +7,9 @@ namespace Taller_2
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void crear_carta()
-        {
-            
-            //crear carta con un buleano
-            
+        
 
-
-        }
-
-        [TestMethod]
-        public void crear_jugador()
-        {
-
-        }
+       
 
         [TestMethod]
         public void cear_barajaDECK()
@@ -28,7 +17,7 @@ namespace Taller_2
             Deck baraja = new Deck();
 
             //si hay carta que es en la list
-            int[] correc_case = { 0, 1, 2, 3, 4, 5 };
+            int[] correc_case = { 1, 2, 3, 4, 5 };
             int[] incorret_case = { 6, 12, -1, -2 };
 
             for (int i = 0; i < correc_case.Length; i++)
@@ -42,10 +31,40 @@ namespace Taller_2
             {
                 Deck barajaChar = new Deck(incorret_case[i]);
 
-                Assert.ThrowsException<Exception>(() => (barajaChar.deckChar.Count < 5));
+                Assert.ThrowsException<Exception>(() => (barajaChar.deckChar.Count < 2));
             }
 
 
+
+        }
+        [TestMethod]
+        public void crear_carta()
+        {
+            /*
+            List<Character> deckChar = new List<Character>();
+            var resultado = baraja;
+            Assert.IsTrue();*/
+            //llamar alas variables
+            Deck baraja = new Deck();
+            List<Character> deckChar = new List<Character>();
+            List<Equip> deckEquip = new List<Equip>();
+            List<SupportSkill> deckSkill = new List<SupportSkill>();
+
+            int[] carater_person = { 1, 2, 3, 4, 5 };
+
+
+            //ejecutar el problema
+            for (int i = 0; i < carater_person.Length; i++)
+            {
+                if (i > deckChar.Count ||deckChar.Count< i)
+                {
+                   
+
+                }
+            
+            }
+
+            //revisar el problema
 
         }
 
@@ -68,9 +87,25 @@ namespace Taller_2
 
         }
 
+
+        [TestMethod]
+        public void Agragar_carta()
+        {
+
+
+        }
+        [TestMethod]
+        public void Batalla()
+        {
+            Player player =new Player();
+
+
+        }
+
         [TestMethod]
         public void cartas_destruidas()
         {
+
 
         }
     }
