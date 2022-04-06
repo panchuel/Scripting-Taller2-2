755d1f5d105d2cae39476ab41c3f9a672ba8f0af
juananre <78058130+juananre@users.noreply.github.com>
2022-04-05 21:00:58 -0500

    s

---
+++ Taller 2/Taller 2/Gacha.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller_2
+{
+    public class Gacha:Card
+    {
+
+           
+        public Gacha() 
+        {
+            //generar cartas
+            Character personaje;
+            Equip equipo;
+            SupportSkill sup;
+
+            int proba;
+            proba = rnd.Next(0, 10);
+
+
+            void gachapon()
+
+            {
+                //probabilidades
+                switch (proba)
+                {
+                    case 0:
+                        personaje = new Character();
+                        personaje.ApplyValues();
+                        genrate_cartas();
+
+                        //character
+                        break;
+                    case 1://
+                        equipo = new Equip();
+                        equipo.ApplyValues();
+                        genrate_cartas();
+
+                        //equip
+                        break;
+
+
+                    case 2: //
+                        sup = new SupportSkill();
+                        sup.ApplyValues();
+                        genrate_cartas();
+
+                        //equip
+                        break;
+
+                    case 3: //
+                        sup = new SupportSkill();
+                        sup.ApplyValues();
+                        genrate_cartas();
+
+
+                        break;
+                    case 4: //
+                        sup = new SupportSkill();
+                        sup.ApplyValues();
+                        genrate_cartas();
+
+
+                        break;
+                    case 5: //
+                        sup = new SupportSkill();
+                        sup.ApplyValues();
+                        genrate_cartas();
+
+
+                        break;
+                    case 6: //
+                        sup = new SupportSkill();
+                        sup.ApplyValues();
+                        genrate_cartas();
+
+
+                        break;
+                    case 7://
+                        equipo = new Equip();
+                        equipo.ApplyValues();
+                        genrate_cartas();
+                        Console.WriteLine(equipo);
+
+                        //equip
+                        break;
+                    case 8://
+                        equipo = new Equip();
+                        equipo.ApplyValues();
+                        genrate_cartas();
+
+                        //equip
+                        break;
+                    case 9:
+                        personaje = new Character();
+                        personaje.ApplyValues();
+                        genrate_cartas();
+
+                        //character
+                        break;
+
+                }
+            }
+
+
+
+            //imprimir
+
+            void genrate_cartas()
+            {
+                double rareza;
+                rareza = rnd.Next(0, 3);
+
+                switch (rareza)
+                {
+                    case 0:
+                        Rarety carta = Rarety.Common;
+
+                        break;
+                    case 1:
+                        Rarety carta2 = Rarety.Rare;
+                        break;
+                    case 2:
+                        Rarety carta3 = Rarety.SuperRare;
+                        break;
+                    case 3:
+                        Rarety carta4 = Rarety.UltraRare;
+                        break;
+
+                }
+
+
+
+
+
+            }
+
+
+
+        }
+
+
+       
+
+    }
+}
--- Taller 2/Taller 2/GameMain.cs
+++ Taller 2/Taller 2/GameMain.cs
@@ -16,11 +16,11 @@ namespace Taller_2
 
             Console.WriteLine("BIENVENIDO A SACA CANAS\n" + "1. Genera una baraja aleatoria         2. Combate contra un adversario\n");
             index = int.Parse(Console.ReadLine());
-            while(index < 1 || index > 2)
+            /*while(index < 1 || index > 2)
             {
                 Console.WriteLine("Opción invalida, intente de nuevo");              
                 index = int.Parse(Console.ReadLine());
-            }
+            }*/
 
             if(index == 1)                                                                                          //Hace las pruebas de que solo se pueda agregar una carta si el CP de esta es menor o igual al CP restante de la baraja 
             {                                                                                                       //y tambien evalua que se reste el CP de la baraja al añadir una carta
@@ -41,6 +41,12 @@ namespace Taller_2
             if(index == 2)
             {
                 Console.WriteLine("Ahora lo implemento, teta");
+            }
+            if (index == 3) 
+            {
+                Console.WriteLine("gacha");
+
+
             }
         } 
         
